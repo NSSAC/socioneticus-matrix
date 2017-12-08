bef45d1055fc7f1259282adaa07ec7e50e1b6066
Parantapa Bhattacharya <paran@vt.edu>
2017-12-08 14:27:01 -0500

    Add dummy controller and agent code

--- socialsim/__init__.py
+++ socialsim/__init__.py
@@ -2,23 +2,40 @@
 SocialSim
 """
 
+import json
+import socket
+from random import randint
+from pprint import pprint
+
 import click
 import logbook
+import gevent
 from gevent.server import StreamServer
 from jsonrpc import JSONRPCResponseManager, Dispatcher
 
 _log = logbook.Logger(__name__)
 
-def rpc_echo(line):
-    return line
+def echo(text):
+    return text
 
-def serve(socket, address):
+def register_event(**event):
+    pprint(event)
+
+def has_round_started():
+    sleep_time = randint(1, 5)
+    print("sleep time: %d" % sleep_time)
+    gevent.sleep(sleep_time)
+    return True
+
+def serve(sock, address):
     """
     Serve this client.
     """
 
     dispatcher = Dispatcher({
-        "echo": rpc_echo
+        "echo": echo,
+        "register_event": register_event,
+        "has_round_started": has_round_started
     })
 
     address_str = ":".join(map(str, address))
@@ -26,19 +43,24 @@ def serve(socket, address):
 
     # We are expecting json only
     # So encoding ascii shoud be sufficient
-    with socket.makefile(mode='r', encoding='ascii') as fobj:
+    with sock.makefile(mode='r', encoding='ascii') as fobj:
         for line in fobj:
             response = JSONRPCResponseManager.handle(line, dispatcher)
-            response = response.json + "\n"
+            response = response.json + "\n" # The newline is mandatory
             response = response.encode("ascii")
-            socket.sendall(response)
+
+            sock.sendall(response)
 
         _log.info("{0} disconnected", address_str)
 
-@click.command()
+@click.group()
 def cli():
+    pass
+
+@cli.command()
+def controller():
     """
-    Example script.
+    Controller process
     """
 
     server = StreamServer(('127.0.0.1', 16000), serve)
@@ -48,3 +70,52 @@ def cli():
     # we use blocking serve_forever() here because we have no other jobs
     _log.notice('Starting echo server on port 16000')
     server.serve_forever()
+
+@cli.command()
+def agent():
+    """
+    Dummy agent.
+
+    For 10 rounds keep sending the canned push event.
+    """
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('127.0.0.1', 16000))
+
+    with sock.makefile(mode="r", encoding="ascii") as fobj:
+        rounds = 10
+        for round_ in range(rounds):
+            msg = {
+                "jsonrpc": "2.0",
+                "id": round_ * 10 + 1,
+                "method": "has_round_started",
+            }
+            msg = json.dumps(msg) + "\n" # The newline is mandatory
+            sock.sendall(msg.encode("ascii"))
+
+            line = fobj.readline()
+            print(line, end="")
+
+            msg = {
+                "jsonrpc": "2.0",
+                "id": round_ * 10 + 2,
+                "method": "register_event",
+                "params": {
+                    "actor": {
+                        "id": 1800460,
+                    },
+                    "repo": {
+                        "id": 111111,
+                    },
+                    "type": "PushEvent",
+                    "created_at": "2015-02-26T00:00:00Z",
+                    "payload": {
+                        "push_id": 585260388,
+                    }
+                }
+            }
+            msg = json.dumps(msg) + "\n" # The newline is mandatory
+            sock.sendall(msg.encode("ascii"))
+
+            line = fobj.readline()
+            print(line, end="")
