2ac3622d8314c01168a32683e4cc293b72bd1e80
Parantapa Bhattacharya <pb@parantapa.net>
2018-01-29 10:51:49 -0500

    Simplify thresholdagent cli

--- matrix/__init__.py
+++ matrix/__init__.py
@@ -2,12 +2,14 @@
 Matrix: CLI Interface
 """
 
+import sys
+
 import click
 import logbook
 
 from .controller import main_controller
 from .simpleagent import main_agent as main_simpleagent
-from .thresholdagent import main_agent_single as main_thresholdagent, \
+from .thresholdagent import main_agent_single as main_thresholdagent_single, \
                             main_agent_multi as main_thresholdagent_multi
 from .initdb import main_initdb
 
@@ -109,34 +111,9 @@ def simpleagent(**kwargs):
               type=click.Path(exists=True, dir_okay=False),
               help="Event database location")
 @click.option("-i", "--agent-id",
-              required=True,
               type=int,
               help="The ID of the agent")
-@click.option("-p", "--nl-prob",
-              default=0.5,
-              help="New repository lookup probability")
-@click.option("-n", "--con-thres",
-              default=2,
-              help="Repository consideration threshold")
-def thresholdagent(**kwargs):
-    """
-    Start a threshold agent process.
-    """
-
-    return main_thresholdagent(**kwargs)
-
-@cli.command()
-@click.option("-a", "--address",
-              envvar=ENVVAR_ADDRESS,
-              required=True,
-              help="Controller address in the format [IP:PORT]")
-@click.option("-e", "--event-db",
-              envvar=ENVVAR_EVENT_DB,
-              required=True,
-              type=click.Path(exists=True, dir_okay=False),
-              help="Event database location")
 @click.option("-I", "--agent-id-range",
-              required=True,
               nargs=2,
               type=int,
               help="The start and stop range of the agent IDs (endpoints are inclusive)")
@@ -146,13 +123,27 @@ def thresholdagent(**kwargs):
 @click.option("-n", "--con-thres",
               default=2,
               help="Repository consideration threshold")
-def thresholdagent_multi(**kwargs):
+def thresholdagent(**kwargs):
     """
-    Start a threshold agent process.
+    Start threshold agent processes.
+
+    Note: either specify a single agent ID using --agent-id,
+    or specify multiple agent IDs using --agent-id-range.
+    Giving both or neither is an error.
     """
 
-    start, stop = kwargs.pop("agent_id_range")
+    agent_id = kwargs.pop("agent_id")
+    agent_id_range = kwargs.pop("agent_id_range")
+
+    if bool(agent_id) == bool(agent_id_range):
+        print("Specifying only one of --agent-id and --agent-id-range is required.", file=sys.stderr)
+        return 1
+
+    if agent_id:
+        kwargs["agent_id"] = agent_id
+        return main_thresholdagent_single(**kwargs)
+
+    start, stop = agent_id_range
     agent_ids = list(range(start, stop + 1))
     kwargs["agent_ids"] = agent_ids
-
     return main_thresholdagent_multi(**kwargs)
