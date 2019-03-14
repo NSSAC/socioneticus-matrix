3301cff9aba4c73a44bad3bc58ec9674ee7b3509
Parantapa Bhattacharya <pb@parantapa.net>
2019-03-14 18:13:54 -0400

    Cleanup Matrix's handling of managing RabbitMQ process

--- matrix/run_rabbitmq.py
+++ matrix/run_rabbitmq.py
@@ -1,13 +1,13 @@
 """
 Run rabbitmq.
 """
-# pylint: disable=subprocess-popen-preexec-fn
 
 import os
+import sys
 import time
 import signal
 from pathlib import Path
-from subprocess import Popen
+from subprocess import Popen, TimeoutExpired
 from contextlib import contextmanager
 import configparser
 
@@ -15,15 +15,23 @@ import logbook
 
 log = logbook.Logger(__name__)
 
-TERM_SIGNALS = ["SIGINT", "SIGTERM", "SIGHUP"]
+TERM_SIGNALS = ["SIGINT", "SIGQUIT", "SIGHUP", "SIGTERM"]
 
-def preexecfn():
+def wait_and_kill(proc, timeout=5.0):
     """
-    Disable handling of the above signals in child.
+    Wait for the proc to finish cleanly or kill.
     """
 
-    for signame in TERM_SIGNALS:
-        signal.signal(getattr(signal, signame), signal.SIG_IGN)
+    try:
+        proc.wait(timeout=timeout)
+    except TimeoutExpired:
+        proc.terminate()
+        try:
+            proc.wait(timeout=5.0)
+        except TimeoutExpired:
+            proc.kill()
+
+    return proc.poll()
 
 @contextmanager
 def epmd_context():
@@ -32,15 +40,15 @@ def epmd_context():
     """
 
     log.info("Starting empd ...")
-    epmd = Popen(["epmd"], preexec_fn=preexecfn)
+    epmd = Popen(["epmd", "-relaxed_command_check"])
     try:
-        time.sleep(1)
         yield epmd
     finally:
         log.info("Shutting down epmd ...")
-        Popen(["epmd", "-kill"], preexec_fn=preexecfn).wait()
+        epmd_kill = Popen(["epmd", "-kill"])
 
-        epmd.wait()
+        wait_and_kill(epmd_kill)
+        wait_and_kill(epmd)
 
 @contextmanager
 def rabbitmq_context():
@@ -49,14 +57,15 @@ def rabbitmq_context():
     """
 
     log.info("Starting rabbitmq-server ...")
-    rabbitmq = Popen(["rabbitmq-server"], preexec_fn=preexecfn)
+    rabbitmq = Popen(["rabbitmq-server"])
     try:
         yield rabbitmq
     finally:
         log.info("Shutting down rabbitmq-server ...")
-        Popen(["rabbitmqctl", "shutdown"], preexec_fn=preexecfn).wait()
+        rabbitmq_shutdown = Popen(["rabbitmqctl", "shutdown"])
 
-        rabbitmq.wait()
+        wait_and_kill(rabbitmq_shutdown, timeout=60)
+        wait_and_kill(rabbitmq)
 
 def cleanup(signame):
     """
@@ -68,59 +77,49 @@ def cleanup(signame):
         Handle the signal.
         """
 
-        log.info(f"Received {signame}")
+        log.info("Received {}", signame)
 
     return do_cleanup
 
-def startup(config_fname, mnesia_base, log_base, hostname, rabbitmq_pid_fname, runner_pid_fname, management_port):
+def startup(config_fname, mnesia_base, hostname, pid_fname):
     """
     Start rabbitmq-server.
     """
 
     os.environ["RABBITMQ_CONFIG_FILE"] = str(config_fname)
     os.environ["RABBITMQ_MNESIA_BASE"] = str(mnesia_base)
-    os.environ["RABBITMQ_LOG_BASE"] = str(log_base)
-    os.environ["RABBITMQ_PID_FILE"] = str(rabbitmq_pid_fname)
+    os.environ["RABBITMQ_LOGS"] = "-"
+    os.environ["RABBITMQ_SASL_LOGS"] = "-"
     os.environ["HOSTNAME"] = hostname
-
-    log.info("Enable managment plugins ...")
-    Popen(["rabbitmq-plugins", "enable", "rabbitmq_management"]).wait()
 
     # Ignore the signals when starting
     for signame in TERM_SIGNALS:
         signal.signal(getattr(signal, signame), signal.SIG_IGN)
 
-    with epmd_context():
-        with rabbitmq_context():
+    with epmd_context() as epmd:
+        with rabbitmq_context() as rabbitmq:
 
-            # Wait for rabbitmq process to start
-            log.info("Waiting for rabbitmq process to start ...")
-            cmd = ["rabbitmqctl", "--timeout", "120", "wait", str(rabbitmq_pid_fname)]
-            Popen(cmd, preexec_fn=preexecfn).wait()
-
-            # Health check
-            log.info("Rabbitmq process health check ...")
-            cmd = ["rabbitmqctl", "node_health_check"]
-            Popen(cmd, preexec_fn=preexecfn).wait()
-
-            # Writing pid to file
-            with open(runner_pid_fname, "wt") as fobj:
-                fobj.write(str(os.getpid()))
+            if epmd.poll() is not None:
+                log.error("Epmd exited prematurely with returncode: {}", epmd.poll())
+                sys.exit(1)
+            if rabbitmq.poll() is not None:
+                log.error("Rabbitmq exited prematurely with returncode: {}", rabbitmq.poll())
+                sys.exit(1)
 
             # Setup handlers for signals
             for signame in TERM_SIGNALS:
                 signal.signal(getattr(signal, signame), cleanup(signame))
 
-            log.info(f"Management UI: http://{hostname}:{management_port}")
-
-            log.info("Waiting for term signal ...")
-            signal.pause()
-            log.info("Term signal received ...")
-
-            log.info(f"Removing runner pid file: {runner_pid_fname}")
-            runner_pid_fname.unlink()
-            log.info(f"Removing rabbitmq pid file: {rabbitmq_pid_fname}")
-            rabbitmq_pid_fname.unlink()
+            # Writing pid to file
+            with open(pid_fname, "wt") as fobj:
+                fobj.write(str(os.getpid()))
+            try:
+                log.info("Waiting for term signal ...")
+                signal.pause()
+                log.info("Term signal received ...")
+            finally:
+                log.info("Removing pid file: {}", pid_fname)
+                pid_fname.unlink()
 
 def main_rabbitmq_start(config_fname, runtime_dir, hostname):
     """
@@ -130,32 +129,24 @@ def main_rabbitmq_start(config_fname, runtime_dir, hostname):
     config_fname = Path(config_fname).absolute()
     runtime_dir = Path(runtime_dir).absolute()
 
-    with open(config_fname, "rt") as fobj:
-        rcfg = configparser.ConfigParser()
-        rcfg.read_string("[default]\n" + fobj.read())
-    management_port = rcfg["default"].get("management.listener.port", 15672)
+    # with open(config_fname, "rt") as fobj:
+    #     rcfg = configparser.ConfigParser()
+    #     rcfg.read_string("[default]\n" + fobj.read())
+    # management_port = rcfg["default"].get("management.listener.port", 15672)
 
     config_fname = config_fname.parent / config_fname.stem
     mnesia_base = runtime_dir / "mnesia"
-    log_base = runtime_dir / "log"
-    rabbitmq_pid_fname = runtime_dir / "rabbitmq.pid"
-    runner_pid_fname = runtime_dir / "run_rabbitmq.pid"
+    pid_fname = runtime_dir / "matrix_rabbitmq.pid"
 
     if not mnesia_base.exists():
-        log.info(f"Creating directory {mnesia_base}")
+        log.info("Creating directory {}", mnesia_base)
         mnesia_base.mkdir(mode=0o700)
-    if not log_base.exists():
-        log.info(f"Creating directory {log_base}")
-        log_base.mkdir(mode=0o700)
 
-    if rabbitmq_pid_fname.exists():
-        log.info(f"Removing existing rabbitmq pid file: {rabbitmq_pid_fname}")
-        rabbitmq_pid_fname.unlink()
-    if runner_pid_fname.exists():
-        log.info(f"Removing existing runner pid file: {runner_pid_fname}")
-        runner_pid_fname.unlink()
+    if pid_fname.exists():
+        log.info("Removing existing pid file: {}", pid_fname)
+        pid_fname.unlink()
 
-    startup(config_fname, mnesia_base, log_base, hostname, rabbitmq_pid_fname, runner_pid_fname, management_port)
+    startup(config_fname, mnesia_base, hostname, pid_fname)
 
 def main_rabbitmq_stop(runtime_dir):
     """
@@ -163,7 +154,7 @@ def main_rabbitmq_stop(runtime_dir):
     """
 
     runtime_dir = Path(runtime_dir).absolute()
-    pid_fname = runtime_dir / "run_rabbitmq.pid"
+    pid_fname = runtime_dir / "matrix_rabbitmq.pid"
 
     with open(pid_fname, "rt") as fobj:
         pid = int(fobj.read())
